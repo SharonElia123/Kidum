4bb6f0175a8c2e9e4974a89375eb1f69b2370395
Sharon Maymon <43923686+SharonElia123@users.noreply.github.com>
2018-10-07 15:00:01 +0300

    Add Files

---
+++ junitTestSuiteSanity.java
@@ -0,0 +1,20 @@
+package KidumOops;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({KidumISRVOops.class,KidumPackOops.class,KidumOrgTrip.class,KidumPackOops2.class})
+
+
+public class junitTestSuiteSanity {
+
+    public junitTestSuiteSanity() {
+
+    }
+
+
+}
